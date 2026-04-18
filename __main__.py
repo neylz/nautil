c60d9ce9cb26ad6275c1cca1c4ed4969e009c26a
Neylz <pacome@neylz.dev>
2026-04-18 16:20:52 +0200

    remove utils dependencies

--- __main__.py
+++
@@ -1,6 +0,0 @@
-from nautil.app.cli import main
-
-
-if __name__ == "__main__":
-
-    main()
--- nautil/source/__init__.py
+++
@@ -1,7 +0,0 @@
-from nautil.source.git import GitSource
-from nautil.source.local import LocalSource
-
-__all__ = [
-    "GitSource",
-    "LocalSource",
-]
